86a203320e9f7048110d2addefcf754518f29b7f
AhHamza <ahmed7okka@gmail.com>
2022-05-05 21:16:34 +0200

    second commit

--- App.js
+++ App.js
@@ -1,27 +1,78 @@
 import { onAuthStateChanged } from "firebase/auth";
-// import Login from "./Components/Users/Login";
 import Register from "./Components/Users/Register";
 import Login from "./Components/Users/Login";
 import { auth } from "./db/Config";
 import { useState, useEffect } from "react";
-// import Cities from "./Components/Cities/Cities";
-// import GuessMyNumber from "./Components/GuessMyNumber";
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+// import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { StyleSheet } from "react-native";
+import Home from "./Pages/Home"
+
+
+const NotUsrStck = createNativeStackNavigator();
+
+function NotUser() {
+    return (
+        <NavigationContainer>
+            <NotUsrStck.Navigator initialRouteName='Sign in'>
+                <NotUsrStck.Screen name='Sign in' component={Login} />
+                <NotUsrStck.Screen name='Register' component={Register} />
+            </NotUsrStck.Navigator>
+        </NavigationContainer>
+    );
+}
+
+const UserStack = createNativeStackNavigator();
+
+function User() {
+    return (
+
+        <NavigationContainer>
+            <UserStack.Navigator>
+                <UserStack.Screen name="Home" initialRouteName="Home" component={Home}>
+
+                </UserStack.Screen>
+            </UserStack.Navigator>
+
+
+
+        </NavigationContainer>
+
+    )
+}
+
+
+
+
 
 export default function App() {
-  useEffect(() => {
-    const unsub = onAuthStateChanged(auth, (user) => setUser(user)); //if (user) is authinticated set(user) -> doesn't have to register since i am already authinticated, else get register tab
+    useEffect(() => {
+        const unsub = onAuthStateChanged(auth, (user) => {
+            setUser(user);
+            setEmail(user.email);
+        }); //if (user) is authinticated set(user) -> doesn't have to register since i am already authinticated, else get register tab
 
-    return () => {
-      unsub();
-    };
-  }, []);
 
-  const [user, setUser] = useState(undefined);
+        return () => {
+            unsub();
+        };
+    }, []);
 
-  return (
-    // user ? <MessagesList /> : <Login /> 
-    <Login/>
-    
-    // <MessagesList />
-  );
+
+    const [user, setUser] = useState(undefined);
+    const [email, setEmail] = useState("");
+
+
+    if (user) {
+        return (
+            <User />
+        );
+    } else {
+        return (
+            <NotUser />
+        );
+    }
 }
+
+

edc8230aef5d6dc53ff71616e0a2d9ad1f853397
abdelghanysd <aabdelghany@singledigits.com>
2022-05-13 14:37:56 +0200

    Fixed adding user inside firestore

--- db/User.js
+++ db/User.js
@@ -5,7 +5,9 @@ const firestoreDB = getFirestore(app);
 
 
 async function addUser(user) {
-    const docRef = await setDoc(collection(firestoreDB, "users",user.id), { id: user.id });
+    // I think this method is not right and instead of using setDoc(collection use setDoc(doc
+    // const docRef = await setDoc(collection(firestoreDB, "users",user.id), { id: user.id });
+    await setDoc(doc(firestoreDB, "users", user.id), user);
 }
 
 async function getUsers() {
